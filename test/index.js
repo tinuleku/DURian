79e0081da187cbc99bcb8669141b34be028df3f3
Tinuleku <etienne.lecouturier@gmail.com>
2015-11-03 19:51:23 +0000

    initial commit

---
+++ test/index.js
@@ -0,0 +1,12 @@
+var mocha = require('mocha');
+var should = require('should');
+
+var SvcUser = require('../services/svc_user.js');
+
+describe("index", function() {
+	describe("applyOperation", function() {
+		it("should", function() {
+			
+		});
+	})
+})
